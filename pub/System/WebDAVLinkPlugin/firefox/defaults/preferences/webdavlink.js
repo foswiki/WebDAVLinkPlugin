6dce075c08f0efc0b16cdcac95686fdaeea40fda
cdot <github@c-dot.co.uk>
2016-10-25 10:17:55 +0100

    Item14106: Converted firefox add-on to a full listin in Mozilla add-ons

--- pub/System/WebDAVLinkPlugin/firefox/defaults/preferences/webdavlink.js
+++ pub/System/WebDAVLinkPlugin/firefox/defaults/preferences/webdavlink.js
@@ -1,3 +1,3 @@
 pref("extensions.webdavlink.executable", "/usr/bin/ooffice");
 // See http://kb.mozillazine.org/Localize_extension_descriptions
-pref("extensions.webdavlink@wikiring.com.description", "chrome://webdavlink/locale/webdavlink.properties");
+pref("extensions.webdavlink@foswiki.org.description", "chrome://webdavlink/locale/webdavlink.properties");
